7f86bb34d3c18c7486fe2f7dde92fb3202e1b512
Ajla Velagic <ajvelagic@gmail.com>
2020-08-18 08:24:22 -0400

    added events

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,26 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-import { Todo } from "../interfaces/todo";
+import { Todo } from '../interfaces/todo';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent implements OnInit {
-  todos: Todo[] = [{
-    task: "fold laundry",
-    completed: false,
-  },
-  {
-    task: "exercise",
-    completed: false,
-  },
-  {
-    task: "bake cookies",
-    completed: true,
-  },];
+  todos: Todo[] = [
+    {
+      task: 'fold laundry',
+      completed: false,
+    },
+    {
+      task: 'exercise',
+      completed: false,
+    },
+    {
+      task: 'bake cookies',
+      completed: true,
+    },
+  ];
 
-  constructor() { }
+  search: string;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  //////////////-ADD NEW TO-DO ITEMS//////////////////////////
+  addTask(form: NgForm) {
+    let newTask: Todo = {
+      task: form.value.task,
+      completed: false,
+    };
+    this.todos.push(newTask);
   }
-};
+
+  // //////////////-REMOVE TO-DO ITEMS//////////////////////////
+  removeTask(index: number) {
+    console.log(this.todos.splice(index, 1));
+  }
+
+  // //////////////-CHANGING COMPLETED PROPERTIES///////////////
+  // completeTask(form: NgForm {
+
+  // }
+
+  // //////////////-SEARCH TO-DO ITEMS//////////////////////////
+  setSearch(form: NgForm) {
+    this.search = form.value.search.trim().toLowerCase();
+  }
+
+  // //////////////-FILTER TO-DO ITEMS//////////////////////////
+  filterTask() {
+    if (!this.search) {
+      return this.todos;
+      // } else {
+      //   return this.todos.filter((item) => {
+      //       let listing = this.todos.toLowerCase().trim();
+      //       return this.listing.includes(this.setSearch);
+      //     });
+    }
+  }
+
+  ngOnInit(): void {}
+}
